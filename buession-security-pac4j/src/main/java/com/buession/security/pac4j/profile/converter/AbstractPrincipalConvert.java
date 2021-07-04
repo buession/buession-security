eeeabcc844e65beba19cfacf0a0ad81e46cc71d8
Yong.Teng <webmaster@buession.com>
2021-07-04 21:21:13 +0800

    删除 PrincipalConvert ThreadLocal 缓存，避免 OOM

--- buession-security-pac4j/src/main/java/com/buession/security/pac4j/profile/converter/AbstractPrincipalConvert.java
+++ buession-security-pac4j/src/main/java/com/buession/security/pac4j/profile/converter/AbstractPrincipalConvert.java
@@ -21,13 +21,14 @@
  * +------------------------------------------------------------------------------------------------+
  * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										|
  * | Author: Yong.Teng <webmaster@buession.com> 													|
- * | Copyright @ 2013-2020 Buession.com Inc.														|
+ * | Copyright @ 2013-2021 Buession.com Inc.														|
  * +------------------------------------------------------------------------------------------------+
  */
 package com.buession.security.pac4j.profile.converter;
 
 import com.buession.beans.BeanResolver;
 import com.buession.beans.DefaultBeanResolver;
+import com.buession.security.pac4j.profile.ProfileUtils;
 import com.buession.security.pac4j.subject.Pac4jPrincipal;
 import org.pac4j.core.profile.CommonProfile;
 import org.slf4j.Logger;
@@ -36,16 +37,12 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 /**
  * @author Yong.Teng
  */
 public abstract class AbstractPrincipalConvert<T> implements PrincipalConvert<T> {
-
-	private ThreadLocal<Map<String, T>> PRINCIPAL_CACHE = ThreadLocal.withInitial(()->new HashMap<>());
 
 	private BeanResolver beanResolver = new DefaultBeanResolver();
 
@@ -79,25 +76,8 @@ public abstract class AbstractPrincipalConvert<T> implements PrincipalConvert<T>
 			return null;
 		}
 
-		CommonProfile profile = optional.get();
-
-		Map<String, T> principals = PRINCIPAL_CACHE.get();
-
-		if(principals != null){
-			T principal = principals.get(profile.getId());
-			if(principal != null){
-				logger.debug("Get user from ThreadLocal.");
-				return principal;
-			}
-		}
-
 		try{
-			T object = getInstance();
-			
-			beanResolver.populate(object, profile.getAttributes());
-			PRINCIPAL_CACHE.get().put(profile.getId(), object);
-
-			return object;
+			return ProfileUtils.convert(optional.get(), getType());
 		}catch(IllegalAccessException e){
 			logger.error("CommonProfile convert to {} error: {}.", getType().getName(), e.getMessage());
 		}catch(InvocationTargetException e){
@@ -109,10 +89,7 @@ public abstract class AbstractPrincipalConvert<T> implements PrincipalConvert<T>
 		return null;
 	}
 
-	protected T getInstance() throws IllegalAccessException, InstantiationException{
-		return getType().newInstance();
-	}
-
+	@SuppressWarnings({"unchecked"})
 	protected Class<T> getType(){
 		return (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
 	}
