09642e6d162110a89d95e1a01dd92008cc57519b
Yong.Teng <webmaster@buession.com>
2021-07-04 21:19:09 +0800

    优化 pac4j Principal 和 ProfileUtils 相关

--- buession-security-pac4j/src/main/java/com/buession/security/pac4j/profile/ProfileUtils.java
+++ buession-security-pac4j/src/main/java/com/buession/security/pac4j/profile/ProfileUtils.java
@@ -22,24 +22,213 @@
  * | Copyright @ 2013-2021 Buession.com Inc.														       |
  * +-------------------------------------------------------------------------------------------------------+
  */
-package com.buession.security.pac4j.annotation;
+package com.buession.security.pac4j.profile;
 
+import com.buession.beans.BeanResolver;
+import com.buession.beans.DefaultBeanResolver;
+import com.buession.core.utils.FieldUtils;
+import com.buession.core.utils.StringUtils;
+import com.buession.core.validator.Validate;
 import com.buession.security.pac4j.subject.Pac4jPrincipal;
 import org.apache.shiro.SecurityUtils;
 import org.pac4j.core.profile.CommonProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 /**
+ * Profile 工具类
+ *
  * @author Yong.Teng
+ * @since 1.2.2
  */
 public class ProfileUtils {
 
+	private final static Logger logger = LoggerFactory.getLogger(ProfileUtils.class);
+
+	/**
+	 * 获取当前登录用户 Profile
+	 *
+	 * @return 当前登录用户 Profile
+	 */
 	public static CommonProfile getCurrent(){
 		Pac4jPrincipal principal = (Pac4jPrincipal) SecurityUtils.getSubject().getPrincipal();
-		Optional<CommonProfile> optional = principal.getProfile();
+		return getProfileFromPac4jPrincipal(principal);
+	}
 
-		return optional.get();
+	/**
+	 * 从 {@link Pac4jPrincipal} 获取用户 Profile
+	 *
+	 * @param principal
+	 *        {@link Pac4jPrincipal}
+	 *
+	 * @return 用户 Profile
+	 */
+	public static CommonProfile getProfileFromPac4jPrincipal(Pac4jPrincipal principal){
+		return principal.getProfile().get();
+	}
+
+	/**
+	 * 从 {@link CommonProfile} 转换为指定对象
+	 *
+	 * @param profile
+	 *        {@link CommonProfile}
+	 * @param type
+	 * 		目标对象类型
+	 * @param <T>
+	 * 		目标对象类型
+	 *
+	 * @return 目标对象
+	 *
+	 * @throws InstantiationException
+	 * 		实例化异常
+	 * @throws IllegalAccessException
+	 * 		权限异常
+	 * @throws InvocationTargetException
+	 * 		反射异常
+	 */
+	public static <T> T convert(CommonProfile profile, Class<T> type) throws InstantiationException,
+			IllegalAccessException, InvocationTargetException{
+		return convert(profile, type, new DefaultBeanResolver());
+	}
+
+	/**
+	 * 从 {@link CommonProfile} 转换为指定对象
+	 *
+	 * @param profile
+	 *        {@link CommonProfile}
+	 * @param type
+	 * 		目标对象类型
+	 * @param beanResolver
+	 * 		Bean 解析器
+	 * @param <T>
+	 * 		目标对象类型
+	 *
+	 * @return 目标对象
+	 *
+	 * @throws InstantiationException
+	 * 		实例化异常
+	 * @throws IllegalAccessException
+	 * 		权限异常
+	 * @throws InvocationTargetException
+	 * 		反射异常
+	 */
+	@SuppressWarnings({"unchecked"})
+	public static <T> T convert(CommonProfile profile, Class<T> type, BeanResolver beanResolver) throws InstantiationException, IllegalAccessException, InvocationTargetException{
+		return convert(profile, type, beanResolver, "id", "realName");
+	}
+
+	/**
+	 * 从 {@link CommonProfile} 转换为指定对象
+	 *
+	 * @param profile
+	 *        {@link CommonProfile}
+	 * @param type
+	 * 		目标对象类型
+	 * @param idFieldName
+	 * 		ID 字段
+	 * @param realNameFieldName
+	 * 		真实姓名字段
+	 * @param <T>
+	 * 		目标对象类型
+	 *
+	 * @return 目标对象
+	 *
+	 * @throws InstantiationException
+	 * 		实例化异常
+	 * @throws IllegalAccessException
+	 * 		权限异常
+	 * @throws InvocationTargetException
+	 * 		反射异常
+	 */
+	public static <T> T convert(CommonProfile profile, Class<T> type, String idFieldName, String realNameFieldName) throws InstantiationException, IllegalAccessException, InvocationTargetException{
+		return convert(profile, type, new DefaultBeanResolver(), idFieldName, realNameFieldName);
+	}
+
+	/**
+	 * 从 {@link CommonProfile} 转换为指定对象
+	 *
+	 * @param profile
+	 *        {@link CommonProfile}
+	 * @param type
+	 * 		目标对象类型
+	 * @param beanResolver
+	 * 		Bean 解析器
+	 * @param idFieldName
+	 * 		ID 字段
+	 * @param realNameFieldName
+	 * 		真实姓名字段
+	 * @param <T>
+	 * 		目标对象类型
+	 *
+	 * @return 目标对象
+	 *
+	 * @throws InstantiationException
+	 * 		实例化异常
+	 * @throws IllegalAccessException
+	 * 		权限异常
+	 * @throws InvocationTargetException
+	 * 		反射异常
+	 */
+	@SuppressWarnings({"unchecked"})
+	public static <T> T convert(CommonProfile profile, Class<T> type, BeanResolver beanResolver, String idFieldName,
+								String realNameFieldName) throws InstantiationException, IllegalAccessException,
+			InvocationTargetException{
+		T instance = type.newInstance();
+
+		beanResolver.populate(instance, profile.getAttributes());
+
+		if(Map.class.isAssignableFrom(type)){
+			if(Validate.hasText(idFieldName)){
+				((Map) instance).put(idFieldName, getId(profile, idFieldName));
+			}
+
+			if(Validate.hasText(realNameFieldName)){
+				((Map) instance).put(realNameFieldName, getRealName(profile, realNameFieldName));
+			}
+		}else{
+			if(Validate.hasText(idFieldName)){
+				Field idField = FieldUtils.getField(type, idFieldName, true);
+
+				if(idField != null){
+					try{
+						FieldUtils.writeField(instance, idFieldName, idField.getType().cast(getId(profile,
+								idFieldName)), true);
+					}catch(Exception e){
+						if(logger.isErrorEnabled()){
+							logger.error("Write {}::{} failure: {}", type.getName(), idFieldName, e.getMessage());
+						}
+					}
+				}
+			}
+
+			if(Validate.hasText(realNameFieldName)){
+				FieldUtils.writeField(instance, realNameFieldName, getRealName(profile, realNameFieldName), true);
+			}
+		}
+
+		return instance;
+	}
+
+	private final static String getId(final CommonProfile profile, final String idField){
+		if(Validate.hasText(idField)){
+			Object id = profile.getAttribute(idField);
+			return id == null ? null : id.toString();
+		}else{
+			return profile.getId();
+		}
+	}
+
+	private final static String getRealName(final CommonProfile profile, final String realNameField){
+		if(Validate.hasText(realNameField)){
+			Object realName = profile.getAttribute(realNameField);
+			return realName == null ? null : realName.toString();
+		}else{
+			return StringUtils.join(profile.getFirstName(), profile.getFamilyName());
+		}
 	}
 
 }
