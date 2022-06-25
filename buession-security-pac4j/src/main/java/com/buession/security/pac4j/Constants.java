250ee622cd6e80416ac495f50174ed3876b3d25c
Yong.Teng <webmaster@buession.com>
2022-06-25 22:40:13 +0800

    新增 pac4j Text 和 Json AjaxRequestResolver

--- buession-security-pac4j/src/main/java/com/buession/security/pac4j/Constants.java
+++ buession-security-pac4j/src/main/java/com/buession/security/pac4j/Constants.java
@@ -21,7 +21,7 @@
  * +------------------------------------------------------------------------------------------------+
  * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										|
  * | Author: Yong.Teng <webmaster@buession.com> 													|
- * | Copyright @ 2013-2020 Buession.com Inc.														|
+ * | Copyright @ 2013-2022 Buession.com Inc.														|
  * +------------------------------------------------------------------------------------------------+
  */
 package com.buession.security.pac4j;
@@ -29,6 +29,7 @@ package com.buession.security.pac4j;
 /**
  * @author Yong.Teng
  */
+@Deprecated
 public interface Constants {
 
 	String CAS = "cas";
---
+++ buession-security-pac4j/src/main/java/com/buession/security/pac4j/http/JsonAjaxRequestResolver.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * =========================================================================================================
+ *
+ * This software consists of voluntary contributions made by many individuals on behalf of the
+ * Apache Software Foundation. For more information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * +-------------------------------------------------------------------------------------------------------+
+ * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										       |
+ * | Author: Yong.Teng <webmaster@buession.com> 													       |
+ * | Copyright @ 2013-2022 Buession.com Inc.														       |
+ * +-------------------------------------------------------------------------------------------------------+
+ */
+package com.buession.security.pac4j.http;
+
+import org.pac4j.core.context.HttpConstants;
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.exception.http.HttpAction;
+import org.pac4j.core.exception.http.RedirectionAction;
+import org.pac4j.core.exception.http.RedirectionActionHelper;
+import org.pac4j.core.exception.http.UnauthorizedAction;
+import org.pac4j.core.exception.http.WithLocationAction;
+import org.pac4j.core.http.ajax.DefaultAjaxRequestResolver;
+import org.pac4j.core.redirect.RedirectionActionBuilder;
+import org.pac4j.core.util.CommonHelper;
+
+/**
+ * JSON way to compute if a HTTP request is an AJAX one.
+ *
+ * @author Yong.Teng
+ * @since 2.0.0
+ */
+public class JsonAjaxRequestResolver extends DefaultAjaxRequestResolver {
+
+	@Override
+	public HttpAction buildAjaxResponse(final WebContext context,
+										final RedirectionActionBuilder redirectionActionBuilder){
+		String url = null;
+
+		if(isAddRedirectionUrlAsHeader()){
+			final RedirectionAction action = redirectionActionBuilder.getRedirectionAction(context).orElse(null);
+			if(action instanceof WithLocationAction){
+				url = ((WithLocationAction) action).getLocation();
+			}
+		}
+
+		if(context.getRequestParameter(FACES_PARTIAL_AJAX_PARAMETER).isPresent() == false){
+			if(CommonHelper.isNotBlank(url)){
+				context.setResponseHeader(HttpConstants.LOCATION_HEADER, url);
+			}
+			throw UnauthorizedAction.INSTANCE;
+		}
+
+		final StringBuilder buffer = new StringBuilder();
+		buffer.append("{\"redirect\":");
+
+		if(CommonHelper.isNotBlank(url)){
+			buffer.append('{');
+			buffer.append("\"url\":\"").append(url).append("\"");
+			buffer.append('}');
+		}else{
+			buffer.append("{}");
+		}
+
+		return RedirectionActionHelper.buildFormPostContentAction(context, buffer.toString());
+	}
+
+}
