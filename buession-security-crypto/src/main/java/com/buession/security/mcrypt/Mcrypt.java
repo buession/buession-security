97e02ec3dca2c217390da01ea028bf9a469162ec
Yong.Teng <webmaster@buession.com>
2024-06-07 14:31:57 +0800

    mcrypt 代码移至 buession-security-crypto，废弃 Mcrypt 和 Mcrypt PasswordGenerator 相关类

---
+++ buession-security-crypto/src/main/java/com/buession/security/mcrypt/Mcrypt.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * =========================================================================================================
+ *
+ * This software consists of voluntary contributions made by many individuals on behalf of the
+ * Apache Software Foundation. For more information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * +-------------------------------------------------------------------------------------------------------+
+ * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										       |
+ * | Author: Yong.Teng <webmaster@buession.com> 													       |
+ * | Copyright @ 2013-2024 Buession.com Inc.														       |
+ * +-------------------------------------------------------------------------------------------------------+
+ */
+package com.buession.security.mcrypt;
+
+import com.buession.security.crypto.Crypto;
+
+/**
+ * 加解密
+ *
+ * @author Yong.Teng
+ */
+@Deprecated
+public interface Mcrypt extends Crypto {
+
+	/**
+	 * 返回请求算法的名称
+	 *
+	 * @return 返回请求算法的名称
+	 */
+	Algo getAlgo();
+
+	/**
+	 * 设置请求算法的名称
+	 *
+	 * @param algo
+	 * 		请求算法的名称
+	 */
+	@Deprecated
+	void setAlgo(final Algo algo);
+
+	/**
+	 * 对象加密
+	 *
+	 * @param object
+	 * 		需要加密的字符串
+	 *
+	 * @return 加密后的字符串
+	 */
+	default String encode(final Object object) {
+		return encrypt(object);
+	}
+
+	/**
+	 * 字符串解密
+	 * 该方法需要提供信息摘要算法支持双向解密才可用
+	 *
+	 * @param cs
+	 * 		要被解密的 char 值序列
+	 *
+	 * @return 解密后的字符串
+	 */
+	default String decode(final CharSequence cs) {
+		return decrypt(cs);
+	}
+
+}
+
---
+++ buession-security-crypto/src/main/java/com/buession/security/mcrypt/Sha512Mcrypt.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * =========================================================================================================
+ *
+ * This software consists of voluntary contributions made by many individuals on behalf of the
+ * Apache Software Foundation. For more information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * +-------------------------------------------------------------------------------------------------------+
+ * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										       |
+ * | Author: Yong.Teng <webmaster@buession.com> 													       |
+ * | Copyright @ 2013-2024 Buession.com Inc.														       |
+ * +-------------------------------------------------------------------------------------------------------+
+ */
+package com.buession.security.mcrypt;
+
+import com.buession.security.crypto.HashCrypto;
+
+import java.nio.charset.Charset;
+import java.security.Provider;
+
+/**
+ * Sha-512 加密对象
+ *
+ * @author Yong.Teng
+ */
+@Deprecated
+public final class Sha512Mcrypt extends AbstractMcrypt implements HashCrypto {
+
+	/**
+	 * 构造函数
+	 */
+	public Sha512Mcrypt() {
+		super(Algo.SHA512);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param provider
+	 * 		信息摘要对象的提供者
+	 */
+	public Sha512Mcrypt(final Provider provider) {
+		super(Algo.SHA512, provider);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param characterEncoding
+	 * 		字符编码
+	 */
+	@Deprecated
+	public Sha512Mcrypt(final String characterEncoding) {
+		super(Algo.SHA512, characterEncoding);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param charset
+	 * 		字符编码
+	 */
+	public Sha512Mcrypt(final Charset charset) {
+		super(Algo.SHA512, charset);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param characterEncoding
+	 * 		字符编码
+	 * @param provider
+	 * 		信息摘要对象的提供者
+	 */
+	public Sha512Mcrypt(final String characterEncoding, final Provider provider) {
+		this(characterEncoding, null, provider);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param charset
+	 * 		字符编码
+	 * @param provider
+	 * 		信息摘要对象的提供者
+	 */
+	public Sha512Mcrypt(final Charset charset, final Provider provider) {
+		this(charset, null, provider);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param characterEncoding
+	 * 		字符编码
+	 * @param salt
+	 * 		加密密钥
+	 */
+	public Sha512Mcrypt(final String characterEncoding, final String salt) {
+		this(characterEncoding, salt, null);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param charset
+	 * 		字符编码
+	 * @param salt
+	 * 		加密密钥
+	 */
+	public Sha512Mcrypt(final Charset charset, final String salt) {
+		this(charset, salt, null);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param characterEncoding
+	 * 		字符编码
+	 * @param salt
+	 * 		加密密钥
+	 * @param provider
+	 * 		信息摘要对象的提供者
+	 */
+	public Sha512Mcrypt(final String characterEncoding, final String salt, final Provider provider) {
+		super(Algo.SHA512, characterEncoding, salt, provider);
+	}
+
+	/**
+	 * 构造函数
+	 *
+	 * @param charset
+	 * 		字符编码
+	 * @param salt
+	 * 		加密密钥
+	 * @param provider
+	 * 		信息摘要对象的提供者
+	 */
+	public Sha512Mcrypt(final Charset charset, final String salt, final Provider provider) {
+		super(Algo.SHA512, charset, salt, provider);
+	}
+
+}
